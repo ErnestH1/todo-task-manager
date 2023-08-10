c95cb1ec58a3b5fdb0cd3820f57a3ffe7460ed1e
ErnestH1 <hansonernest7@gmail.com>
2023-08-10 11:53:59 +0300

    models

---
+++ api/model.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String)
+    owner_id = Column(Integer, index=True)
+
+class CompletedTask(Base):
+    __tablename__ = "completed_tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    task_id = Column(Integer, ForeignKey("tasks.id"))  
+    completed_by = Column(Integer, ForeignKey("users.id"))  
